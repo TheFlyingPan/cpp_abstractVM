f6409b2ea5720dc8071e8d5d804561daa52f96b7
ET <Alsushi@users.noreply.github.com>
2020-01-03 14:43:21 +0100

    [ADD] - function dictionnary

---
+++ instructions.cpp
@@ -0,0 +1,21 @@
+
+
+function_dictionary = {
+        "push v":pushFunction,
+        "pop":popFunction,
+        "error":errorFunction,
+        "clear":clearFunction,
+        "dup":dupFunction,
+        "swap":swapFunction,
+        "dump":dumpFunction,
+        "assert v":assertFunction,
+        "add":addFunction,
+        "sub":subFunction,
+        "mul":mulFunction,
+        "div":divFunction,
+        "mod":modFunction,
+        "load v":loadFunction,
+        "store v":storeFunction,
+        "print":printFunction,
+        "exit":exitFunction,
+    }
