fece2cde285bc8e0d3754d5a116e788cee7dee56
Aurelie Dantec <aurelie.dantec@epitehc.eu>
2020-01-03 10:53:53 +0100

    [add]: IOperand.cpp, IOperand.hpp

---
+++ IOperand/IOperand.cpp
@@ -0,0 +1,38 @@
+#include "IOperand.hpp"
+
+ std :: string toString() // string that represents the instance
+ {
+
+ };
+
+ eOperandType getType() // returns the type of instance
+ {
+
+ };
+
+ IOperand* operator+( const IOperand & rhs ) // sum
+ {
+
+ };
+
+ IOperand* operator-( const IOperand & rhs ) // difference
+ {
+
+ };
+
+ IOperand* operator*( const IOperand & rhs ) // product
+ {
+
+ };
+
+ IOperand* operator/( const IOperand & rhs ) // quotient
+ {
+
+ };
+
+ IOperand* operator%( const IOperand & rhs ) // modulo
+ {
+
+ };
+
+ ~IOperand() {}
