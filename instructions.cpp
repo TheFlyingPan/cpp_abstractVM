60a13ff45ab875f130c53812cd754cc6781743f9
ET <splitsushi@users.noreply.github.com>
2020-01-06 18:44:09 +0100

    [mod] - WIP dictionnary MAP - partial syntax - need better header

--- instructions.cpp
+++ instructions.cpp
@@ -13,7 +13,115 @@ using namespace std;
 //std::cout<<grade_list.begin()->first<<endl;
 // Should be A
 //std::cout<<grade_list.begin()->second<<endl;
+
+
+void commentsFunction(){
+    //donothing;
+}
+
+void pushFunction(stack <int> stack, int value){
+    stack.push(value);
+}
+
+void popFunction(stack <int> stack)){
+    if(!stack.empty()){
+        stack.pop();
+    } else {
+        //error;
+        break;
+    }
+}
+
+void errorFunction(){
+    cout << "error";
+    break;
+}
+
+void clearFunction(stack <int> stack){
+    stack.clear;
+}
+
+void dupFunction(stack <int> stack){
+    //value
+    if(!stack.empty()){
+        break;
+    } else {
+        stack.pop();
+    }
+}
+
+void swapFunction(){
+    //copy = value1
+    //value2 = value1
+    //value1 = copy
+}
+void dumpFunction(stack <int> stack){
+    while (!stack.empty()) { 
+        cout << '\t' << stack.top(); 
+        stack.pop(); 
+    } 
+    cout << '\n';
+}
+
+void assertFunction(){
+
+}
+
+void addFunction(stack <int> stack){
+    stack.pop();
+    stack.pop();
+}
+
+void subFunction(){
+
+}
+
+void mulFunction(){
+
+}
+
+void divFunction(){
+
+}
+
+void modFunction(){
+
+}
+
+void loadFunction(){
+
+}
+
+void storeFunction(stack <int> stack){
+
+}
+
+void printFunction(stack <int> stack){
+    top = stack.top();
+    if (top != null) {
+        cout << '\t' << top;
+    }
+    showstack(stack);
+}
+
+void exitFunction(stack <int> stack){
+    //kill program
+    //break;
+}
+
+void showstack(stack <int> stack) 
+{ 
+    while (!stack.empty()) 
+    { 
+        cout << '\t' << stack.top(); 
+        stack.pop(); 
+    } 
+    cout << '\n'; 
+}
+
+void main(stack <int> stack, string input)
 {
+
 std::map <string, string> instructionsCmd;
         instructionsCmd["comments"] = 'commentsFunction';
         instructionsCmd["push"] = 'pushFunction';
@@ -33,114 +141,8 @@ std::map <string, string> instructionsCmd;
         instructionsCmd["store"] = 'storeFunction';
         instructionsCmd["print"] = 'printFunction';
         instructionsCmd["exit"] = 'exitFunction';
-};
 
-void commentsFunction(){
-    //donothing;
-}
 
-void pushFunction(int value){
-    stack.push(value);
-}
-
-void popFunction{
-    if(!stack.empty()){
-        stack.pop();
-    } else {
-        //error;
-        break;
-    }
-}
-
-void errorFunction{
-    cout << "error";
-    break;
-}
-
-void clearFunction{
-    stack.clear;
-}
-
-void dupFunction{
-    //value
-    if(!stack.empty()){
-        break;
-    } else {
-        stack.pop();
-    }
-}
-
-void swapFunction{
-    //copy = value1
-    //value2 = value1
-    //value1 = copy
-}
-void dumpFunction{
-    while (!stack.empty()) { 
-        cout << '\t' << stack.top(); 
-        stack.pop(); 
-    } 
-    cout << '\n';
-}
-
-void assertFunction{
-
-}
-
-void addFunction{
-    stack.pop();
-    stack.pop();
-}
-
-void subFunction{
-
-}
-
-void mulFunction{
-
-}
-
-void divFunction{
-
-}
-
-void modFunction{
-
-}
-
-void loadFunction{
-
-}
-
-void storeFunction{
-
-}
-
-void printFunction{
-    top = stack.top();
-    if (top != null) {
-        cout << '\t' << top;
-    }
-    showstack(s);
-}
-
-void exitFunction{
-    //kill program
-    break;
-}
-
-void showstack(stack <int> s) 
-{ 
-    while (!stack.empty()) 
-    { 
-        cout << '\t' << stack.top(); 
-        stack.pop(); 
-    } 
-    cout << '\n'; 
-}
-
-void main()
-{
     cout << "ok";
     stack <int> stack;
 }
