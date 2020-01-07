e0c628189594953f1a5f85b2af6c4ddbc963b555
auré <git@git.epitech.eu:/aurelien.poirier@epitech.eu>
2020-01-07 15:46:39 +0100

    [MOD] - Changed Parser from IO folder to chipset class

--- AbstractVM/Chipset/Chipset.hpp
+++ AbstractVM/Chipset/Chipset.hpp
@@ -1,4 +1,16 @@
+#ifndef __CHIPSET_HPP__
+#define __CHIPSET_HPP__
+
+using namespace std;
+
 class Chipset
 {
-    
+public:
+    Chipset();
+    ~Chipset();
+    void getWords(string line) const;
+    void fromFile(const char* argv) const;
+    void getVar(string line) const;
 };
+
+#endif
--- AbstractVM/IO/Parser/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Parse.cpp"
-#include <iostream>
-
-int main(int argc, char** argv)
-{
-    Parse swag;
-    swag.fromFile(argv[1]);
-    return 0;
-}
